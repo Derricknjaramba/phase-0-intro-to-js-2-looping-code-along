aa87208e26c55f1a80e671bed65a983917190785
Derricknjaramba <njarambaderrick@gmail.com>
2024-07-07 15:09:26 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    let messages = []; 
+    
+    for (let i = 0; i < namesArray.length; i++) {
+      messages.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+     
+}
+    return messages;
+  }
+  
+  
+  let names = ["Guadalupe", "Ollie", "Aki"];
+  let event = 'birthday';
+
+  let cards = writeCards(names, event);
+console.log(cards);
+
+function countDown(startingNumber) {
+    let currentNumber = startingNumber;
+    
+    while (currentNumber >= 0) {
+      console.log(currentNumber);
+      currentNumber--;
+    }
+  }
+  
+  
+  countDown(10);
